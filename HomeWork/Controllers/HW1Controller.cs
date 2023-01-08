4b82defe9ce886b8587ba7c541687fadcfc1b6d9
MaiePu <vickexe@gmail.com>
2023-01-08 16:49:52 +0800

    1

---
+++ HomeWork/Controllers/HW1Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HomeWork.Controllers
+{
+    public class HW1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
